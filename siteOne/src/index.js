2d7e9b73c33f7127d6c51001dfb7fc01d74f952f
Kostya <helmcoil@gmail.com>
2020-05-28 23:52:13 +0300

    one more jsx

--- siteOne/src/index.js
+++ siteOne/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 
 const wrapper = document.getElementById('app');
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<div>WorkingCopy</div>, wrapper);
+ReactDOM.render(<div>Working</div>, wrapper);
