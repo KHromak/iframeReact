eb7e8b1dec1bbb015acdf1e7c19bac61a6625d0d
Kostya <helmcoil@gmail.com>
2020-05-29 01:18:12 +0300

    simple store for siteTwo

--- siteTwo/src/index.js
+++ siteTwo/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import App from './components/App'
+import rootReducer from './reducers'
+
+// import './index.css'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const wrapper = document.getElementById('app');
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<div>Working2</div>, wrapper);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , wrapper);
