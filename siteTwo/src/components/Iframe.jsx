21a86b302d4a89a260b0ba3ce4948721af5eb351
Kostya <helmcoil@gmail.com>
2020-05-31 08:12:41 +0300

    del refs

--- siteTwo/src/components/Iframe.jsx
+++ siteTwo/src/components/Iframe.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const Iframe = ({ src, height, width, iframeRef }, props) => {
 
   return (
     <div>
-      <iframe src={src} height={height} width={width} id={'iframe'} ref={iframeRef} id={'the_iframe'} />
+      <iframe src={src} height={height} width={width} />
     </div>
   )
 };
